9c7b38e05d8a92b576a1a4b2a7f104ab4fedd134
Seann Bryan Timotius <seannbryanlin@gmail.com>
2025-04-22 23:12:57 +0800

    new main

--- JS/recipe3.js
+++ JS/recipe3.js
@@ -1,11 +1,27 @@
+window.addEventListener('DOMContentLoaded', () => {
+  document.body.classList.add('loaded');
+});
 
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
+window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
+  var navbar = document.getElementsByClassName("navbar")[0];
+
   if (prevScrollpos > currentScrollPos) {
-    document.getElementsByClassName("navbar").style.top = "0";
+    navbar.style.top = "0";
   } else {
-    document.getElementsByClassName("navbar").style.top = "-50px";
+    navbar.style.top = "-65px";
   }
+
   prevScrollpos = currentScrollPos;
-}
+};
+
+const homeBtn = document.querySelector('.home-button');
+
+homeBtn.addEventListener('mouseenter', () => {
+    homeBtn.classList.add('pulse');
+});
+
+homeBtn.addEventListener('animationend', () => {
+    homeBtn.classList.remove('pulse');
+});
